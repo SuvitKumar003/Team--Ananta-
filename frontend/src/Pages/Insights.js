06d5ed5536d42367b4cf9eb83143ef432d3053ca
Krmjn09 <mahajankriti672@gmail.com>
2025-10-01 22:30:10 +0530

    Commit changes

--- frontend/src/Pages/Insights.js
+++ frontend/src/Pages/Insights.js
@@ -34,7 +34,7 @@ const Insights = () => {
         category: 'all',
       });
 
-      const API_BASE = 'https://log-analyzer-dpy8.onrender.com/api';
+      const API_BASE = 'http://localhost:5000/api';
 
       useEffect(() => {
         fetchAILogs();
